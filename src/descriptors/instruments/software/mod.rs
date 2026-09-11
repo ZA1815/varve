daf5440f6d8ff287049c613ba9a4c7da160bd430
Zaid Ahmed <zaahme18@gmail.com>
2026-09-11 11:07:35 -0400

    feat(descriptors/instruments/software): Add optimization as another instrument

--- src/descriptors/instruments/software/mod.rs
+++ src/descriptors/instruments/software/mod.rs
@@ -3,10 +3,11 @@ pub mod api;
 pub mod dependency;
 pub mod modularization;
 pub mod naming;
+pub mod optimization;
 pub mod resilience;
 pub mod testing;
 
-use crate::descriptors::instruments::software::{abstraction::AbstractionInstrument, api::ApiInstrument, dependency::DependencyInstrument, modularization::ModularizationInstrument, naming::NamingInstrument, resilience::ResilienceInstrument, testing::TestingInstrument};
+use crate::descriptors::instruments::software::{abstraction::AbstractionInstrument, api::ApiInstrument, dependency::DependencyInstrument, modularization::ModularizationInstrument, naming::NamingInstrument, optimization::OptimizationInstrument, resilience::ResilienceInstrument, testing::TestingInstrument};
 
 pub enum SoftwareInstrument {
     Abstraction(AbstractionInstrument),
@@ -14,6 +15,7 @@ pub enum SoftwareInstrument {
     Dependency(DependencyInstrument),
     Modularization(ModularizationInstrument),
     Naming(NamingInstrument),
+    Optimization(OptimizationInstrument),
     Resilience(ResilienceInstrument),
     Testing(TestingInstrument)
 }
