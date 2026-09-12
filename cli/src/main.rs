2d9176bd61fed7c233f8e8d375d7f10e14056506
ZA1815 <zaahme18@gmail.com>
2026-09-12 17:17:11 -0400

    feat(cli): Start enumerating commands in the CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,1 +1,29 @@
-fn main() {}
+use clap::{CommandFactory, Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "varve", version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Command>
+}
+
+#[derive(Subcommand)]
+pub enum Command {
+    Deposit,
+    Stream,
+    Dossier,
+    Profile,
+    Acquire,
+    Publish
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    if let Some(command) = cli.command {
+        
+    }
+    else {
+        Cli::command().print_help().unwrap();
+    }
+}
